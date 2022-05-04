afeee17b5789f9e7220a1a15840f6878e2f949ff
Hailey Bates <hailey.bates001@gmail.com>
2022-05-03 22:46:18 -0400

    Added more values for table

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,15 +12,31 @@ INSERT INTO role
     (title, salary, department_id)
 VALUES
     ("Data Analyst", 80000, 1),
+    ("Sales Director", 105000, 1),
+    ("Salesman", 60000, 1),
     ("Marketing Director", 100000, 2),
+    ("Public Relations Manager", 95000, 2),
+    ("Design Tech", 77000, 2),
     ("Lawyer", 140000, 3),
-    ("Senior Developer", 100000, 4);
+    ("Researcher", 50000, 3),
+    ("Legal Assistant", 64000, 3),
+    ("Senior Developer", 100000, 4),
+    ("Web Designer", 75000, 4),
+    ("Engineer", 140000, 4);
 
 INSERT INTO employees
     (first_name, last_name, role_id)
 VALUES
     ("Mike", "Myers", 1),
+    ("Adam", "Shaw", 1),
+    ("David", "Perez", 1),
     ("Sam", "Smith", 2),
+    ("Judy", "Campos", 2),
+    ("Melissa", "Creed", 2),
     ("Quinn", "Denver", 3),
+    ("Kayla", "Swindle", 3),
+    ("Riley", "Knight", 3),
+    ("Eddie", "Ramirez", 4),
+    ("Steven", "McIntyre", 4),
     ("Wendy", "Abbot", 4);
     
